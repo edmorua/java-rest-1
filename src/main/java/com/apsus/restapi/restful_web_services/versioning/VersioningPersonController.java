ebe0c23fec9db6c5fa5c2ba84c6c058805350d59
edmorua <eduardo.r.morua@gmail.com>
2025-05-06 07:37:08 -0600

    add header versioning

--- src/main/java/com/apsus/restapi/restful_web_services/versioning/VersioningPersonController.java
+++ src/main/java/com/apsus/restapi/restful_web_services/versioning/VersioningPersonController.java
@@ -2,6 +2,8 @@ package com.apsus.restapi.restful_web_services.versioning;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +17,23 @@ public class VersioningPersonController {
 	public Person getSecondVersionOfPerson() {
 
 		return new PersonV2(new Name("Eduardo", "Ruiz"));
+	}
+
+	@GetMapping("/person")
+	public Person getPerson(@RequestParam String version) {
+		return switch (version) {
+			case "1" -> new PersonV1("Eduardo Ruiz");
+			case "2" -> new PersonV2(new Name("Eduardo", "Ruiz"));
+			default -> throw new IllegalArgumentException("Version not supported");
+		};
+	}
+
+	@GetMapping(path = "/person-header")
+	public Person getPersonByHeader(@RequestHeader("X-API-VERSION") String version) {
+		return switch (version) {
+			case "1" -> new PersonV1("Eduardo Ruiz");
+			case "2" -> new PersonV2(new Name("Eduardo", "Ruiz"));
+			default -> throw new IllegalArgumentException("Version not supported");
+		};
 	}
 }
