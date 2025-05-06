8d66e16c710d14f5e51eca1ad6930fee9aa36a2a
edmorua <eduardo.r.morua@gmail.com>
2025-05-06 06:53:15 -0600

    add internalitation

--- src/main/java/com/apsus/restapi/restful_web_services/hello/HelloWorldController.java
+++ src/main/java/com/apsus/restapi/restful_web_services/hello/HelloWorldController.java
@@ -1,20 +1,35 @@
 package com.apsus.restapi.restful_web_services.hello;
 
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 @RequestMapping("/hello")
 public class HelloWorldController {
+
+	private MessageSource messageSource;
+
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 
 	@GetMapping()
 	public String hello() {
 		return "Hello World";
 	}
 
+	@GetMapping(path = "/international")
+	public String helloInternational() {
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+	}
 
 	@GetMapping(path = "/bean")
 	public HelloWorldBean helloBean() {
